01094edc5aa749a9886b61843f579e073de25b73
Vann4 <1vetlan1@mail.ru>
2023-09-23 21:42:57 +0300

    Доработка кода

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import math
 a = int(input(f'\nВведите переменную а: '))
 b = int(input('Введите переменную b: '))
 c = int(input('Введите переменную c: '))
-x = ''
-D = b**2 - 4*a*c
 
 D = b**2 - 4*a*c
+
+print('\nДискриминант равен:', D)
+
+if D == 0:
+    x = b+b**2/2*a
+    print(f'Ответ уравнения: {x}')
+
+if D<0:
+    print('Уравнение не имеет корней')
+
 if D > 0:
-    x1=(-b+math.sqrt(D))/(2*a)
-    x2=(-b-math.sqrt(D))/(2*a)
-    print('Дискриминант:',D)
+    x1=(b+math.sqrt(D))/(2*a)
+    x2=(b-math.sqrt(D))/(2*a)
     print(f'Уравнение имеет 2 корня: x1={x1}, x2={x2}')
->>>>>>> c63cfa8077ff9407c23ed655f2104fe5e6d34fc9
